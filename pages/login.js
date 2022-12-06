d77c6ac3b4355e648c547cd1436f04e27e8e69f9
Safeer <safeeranwar123@gmail.com>
2022-12-06 19:12:55 +0530

    login and signup page

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <section className="">
+        <section>
             <div className="px-6 h-full text-gray-800">
                 <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
                     <div className="flex justify-center grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12">
